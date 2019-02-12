da49b096493a9f7d367ba6dfd6dfb53efbb195a1
Nupendra Verma <shaurycool55@gmail.com>
2019-02-12 15:26:53 +0530

    Initial Commit

---
+++ Drawer_MenuTests/Drawer_MenuTests.swift
@@ -0,0 +1,34 @@
+//
+//  Drawer_MenuTests.swift
+//  Drawer_MenuTests
+//
+//  Created by PAL-MAC on 12/02/19.
+//  Copyright © 2019 PAL-MAC. All rights reserved.
+//
+
+import XCTest
+@testable import Drawer_Menu
+
+class Drawer_MenuTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
