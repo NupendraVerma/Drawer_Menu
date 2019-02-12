9e3e8de051b0ca580723dbbac81f2119debbb226
Nupendra Verma <shaurycool55@gmail.com>
2019-02-12 15:45:48 +0530

    Add files via upload

---
+++ Drawer_Menu/Table_data.swift
@@ -0,0 +1,26 @@
+//
+//  Table_data.swift
+//  Navigation_Menu
+//
+//  Created by PAL-MAC on 23/01/19.
+//  Copyright © 2019 PAL-MAC. All rights reserved.
+//
+
+import UIKit
+
+class Table_data: UITableViewCell {
+
+    @IBOutlet weak var lbl_title: UILabel!
+    @IBOutlet weak var Img_icon: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
